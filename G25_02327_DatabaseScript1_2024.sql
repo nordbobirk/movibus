2d5fe1a673ab8aac3bde9215e955f5d5daea88b5
Birk Nordbo <36029360+nordbobirk@users.noreply.github.com>
2024-11-13 17:33:31 +0100

    population

--- G25_02327_DatabaseScript1_2024.sql
+++ G25_02327_DatabaseScript1_2024.sql
@@ -3,12 +3,12 @@ create database movibus;
 use movibus; 
 
 create table bus_line (
-line_name varchar(4),
+line_name varchar(4) not null,
 primary key(line_name)
 );
 
 create table passenger ( 
-card_id char(10),
+card_id char(10) not null,
 email varchar(30) not null,
 first_name varchar(30) not null,
 last_name varchar(30) not null,
@@ -16,14 +16,14 @@ primary key(card_id)
 );
 
 create table phone_number (
-phone_number char(8),
-card_id char(10),
+phone_number char(8) not null,
+card_id char(10) not null,
 primary key(phone_number, card_id),
 foreign key(card_id) references passenger(card_id) on delete cascade
 );
 
 create table address (
-card_id char(10),
+card_id char(10) not null,
 street_name varchar(50) not null,
 civic_number varchar(10) not null,
 zip_code varchar(10) not null,
@@ -34,21 +34,15 @@ foreign key(card_id) references passenger(card_id) on delete cascade
 
 create table bus_stop (
 stop_name varchar(30) not null,
-latitude char(9),
-longitude char(9),
+latitude char(9) not null,
+longitude char(9) not null,
 primary key(latitude, longitude)
 );
 
-create table takes (
-card_id char(10),
-start_time datetime,
-primary key(card_id, start_time)#, #this foreign key constraint cant be created because the bus_ride table is not yet created. we add this contraint later
-#foreign key(start_time) references bus_ride(start_time) on delete cascade
-);
-
 create table bus_ride (
-card_id char(10),
-start_time datetime,
+card_id char(10) not null,
+line_name varchar(4) not null,
+start_time datetime not null,
 end_time datetime,
 first_stop_latitude char(9) not null,
 first_stop_longitude char(9) not null,
@@ -57,54 +51,43 @@ last_stop_longitude char(9),
 primary key(card_id, start_time),
 foreign key(first_stop_latitude, first_stop_longitude) references bus_stop(latitude, longitude) on delete cascade,
 foreign key(last_stop_latitude, last_stop_longitude) references bus_stop(latitude, longitude) on delete set null,
-foreign key(card_id) references takes(card_id) on delete cascade
+foreign key(card_id) references passenger(card_id) on delete cascade,
+foreign key(line_name) references bus_line(line_name) on delete cascade
 );
 
-# add missing foreign key constraint to takes table after creating bus_ride
-alter table takes add foreign key(start_time) references bus_ride(start_time) on delete cascade;
-
 create table stops_at ( 
-latitude char(9),
-longitude char(9),
-line_name varchar(4),
-stop_index int,
+latitude char(9) not null,
+longitude char(9) not null,
+line_name varchar(4) not null,
+stop_index int not null,
 primary key(latitude, longitude, line_name),
 foreign key(latitude, longitude) references bus_stop(latitude, longitude) on delete cascade,
 foreign key(line_name) references bus_line(line_name) on delete cascade
-); 
-
-create table rides_on (
-card_id char(10),
-start_time datetime,
-line_name varchar(4),
-primary key(card_id, start_time, line_name),
-foreign key(card_id, start_time) references bus_ride(card_id, start_time) on delete cascade,
-foreign key(line_name) references bus_line(line_name) on delete cascade
 );
 
+insert busline values (
+"500S",
+"350A",
+"300S",
+"700D",
+"690E",
+"420S",
+"860R",
+"105T",
+"550H",
+"280K"
+);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+insert into passenger (card_id, email, first_name, last_name) values
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+();
