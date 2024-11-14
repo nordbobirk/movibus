7cdfe9c73787a5e942107abae3e254af2d43b19a
Birk Nordbo <36029360+nordbobirk@users.noreply.github.com>
2024-11-14 01:56:35 +0100

    fix: AddStopToLine

--- G25_02327_DatabaseScript2_2024.sql
+++ G25_02327_DatabaseScript2_2024.sql
@@ -12,24 +12,19 @@ use movibus;
 # a trigger that prevents inserting a ride starting and ending at the same stop or at a stop not served by that line
 # illustrative examples of all of the above
 
+#################################################################################################
+
 # Show the ID of the passengers who took a ride from the first stop of a given line
-# Instead of writing a query for a single line we create a function to get these passengers from any line
-drop function if exists PassengersFromFirstStop;
-DELIMITER //
-create function PassengersFromFirstStop(line_name varchar(4)) returns char(10)
-begin
+# we demonstrate this query with the bus line 350A
+set @q1_line_name = "350A";
+
 # here we use an inner join and NOT natural join, because attributes that are conceptually the same have different names
-return (select card_id from bus_ride join stops_at where 
+select card_id from bus_ride join stops_at where 
 bus_ride.line_name = stops_at.line_name and 
-bus_ride.line_name = line_name and 
+bus_ride.line_name = @q1_line_name and 
 first_stop_latitude = latitude and 
 first_stop_longitude = longitude and
-stop_index = 1);
-end//
-DELIMITER ;
-
-# This can be used thus
-select PassengersFromFirstStop("350A");
+stop_index = 1;
 
 #################################################################################################
 
@@ -42,6 +37,8 @@ return (select max(stop_index) from stops_at where stops_at.line_name = line_nam
 end//
 DELIMITER ;
 
+#################################################################################################
+
 # A procedure that gets the coordinates of the last stop of a line
 drop procedure if exists LastStopCoordinates;
 DELIMITER //
@@ -52,10 +49,10 @@ select longitude into last_stop_longitude from stops_at where stops_at.line_name
 end//
 DELIMITER ;
 
-#define x char(9);
-call LastStopCoordinates("500S");
+#################################################################################################
 
 # A function that tells us whether a stop is served by some line
+drop function if exists StopServedByLine;
 DELIMITER //
 create function StopServedByLine(stop_latitude char(9), stop_longitude char(9), line_name varchar(4)) returns boolean
 begin
@@ -63,15 +60,23 @@ return exists (select * from stops_at where stops_at.line_name = line_name and s
 end//
 DELIMITER ;
 
+select StopServedByLine("55.846501", "12.414829", "500S") as servedByLine;
+
+#################################################################################################
+
 # A procedure that given a line and a stop adds the stop to that line after the last stop if the stop is not already served by that line
+drop procedure if exists AddStopToLine;
 DELIMITER //
-create procedure AddStopToLine(in line_name varchar(4), in stop_latitude char(9), in stop_longtitide char(9), in stop_name varchar(20))
+create procedure AddStopToLine(in line_name varchar(4), in stop_latitude char(9), in stop_longitude char(9))
 begin
 declare last_stop_index int;
 select LastStopIndex(line_name) into last_stop_index;
 if StopServedByLine(stop_latitude, stop_longitude, line_name)
 then signal sqlstate "HY000" set mysql_errno = 1525, message_text = "stop already served by line";
 end if;
-insert stops_at values (latitude = stop_latitude, longitude = stop_longitude, stop_name = stop_name, stop_index = last_stop_index);
+insert stops_at (latitude, longitude, stop_index, line_name) values (stop_latitude, stop_longitude, (last_stop_index + 1), line_name);
 end//
 DELIMITER ;
+
+select * from stops_at natural join bus_stop order by line_name asc, stop_index asc;
+call AddStopToLine("300S", "55.715321", "12.337132");
